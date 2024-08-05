fecc5a20444fba68ab3463e9090fb0ed36ca1438
rishabhag18 <18r.agarwal@gmail.com>
2024-08-05 17:22:20 +0530

    image loading issue resolved

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,17 +1,18 @@
+import style from "../components/NetflixCards.module.css";
 const SeriesCard = ({ data }) => {
   return (
-    <li className="card">
+    <li className={style.card}>
       <div>
         <img src={data.img_url} alt="series-image" width="40%" height="40%" />
       </div>
-      <div className="card-content">
+      <div className={style["card-content"]}>
         <h2>{data.name}</h2>
-        <h3>Rating: <span className={data.rating>8.5?"super-hit":"average"}>{data.rating}</span></h3>
+        <h3>Rating: <span className={data.rating>8.5?style['super-hit']:style["average"]}>{data.rating}</span></h3>
         <p>{data.description}</p>
         <p>Genre: {data.genre.join(", ")}</p>
         <p>Cast: {data.cast.join(", ")}</p>
         <a href={data.watch_url} target="_blank">
-          <button className="watchnow-button">Watch Now</button>
+          <button className={style["watchnow-button"]}>Watch Now</button>
         </a>
       </div>
     </li>
